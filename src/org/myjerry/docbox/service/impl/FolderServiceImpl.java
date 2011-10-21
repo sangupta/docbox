bec64211717559d5feb1e6812a2081722bb18700
Sandeep Gupta <sandy.pec@gmail.com>
2011-10-21 17:00:57 +0530

    Added copyright header.

--- src/org/myjerry/docbox/service/impl/FolderServiceImpl.java
+++ src/org/myjerry/docbox/service/impl/FolderServiceImpl.java
@@ -1,3 +1,23 @@
+/**
+ * myJerry | DocBox
+ * Copyright (C) 2011, myJerry Developers
+ * http://www.myjerry.org/docbox
+ * 
+ * The file is licensed under the the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
 package org.myjerry.docbox.service.impl;
 
 import java.util.ArrayList;
@@ -16,6 +36,11 @@ import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 
+/**
+ * 
+ * @author sangupta
+ * @created 21 Oct 2011
+ */
 public class FolderServiceImpl implements FolderService {
 	
 	private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
--- src/org/myjerry/docbox/web/handlers/CreateFolderHandler.java
+++ src/org/myjerry/docbox/web/handlers/CreateFolderHandler.java
@@ -1,3 +1,23 @@
+/**
+ * myJerry | DocBox
+ * Copyright (C) 2011, myJerry Developers
+ * http://www.myjerry.org/docbox
+ * 
+ * The file is licensed under the the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
 package org.myjerry.docbox.web.handlers;
 
 import java.io.IOException;
@@ -11,6 +31,11 @@ import org.myjerry.docbox.service.FolderService;
 import org.myjerry.docbox.web.ModelAndView;
 import org.myjerry.docbox.web.RequestHandler;
 
+/**
+ * 
+ * @author sangupta
+ * @created 21 Oct 2011
+ */
 public class CreateFolderHandler implements RequestHandler {
 	
 	private FolderService folderService = Beans.getBean(BeanNames.FOLDER_SERVICE);
